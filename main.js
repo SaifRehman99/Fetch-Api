c116b7ba5d0307b900cae15e90044a9b05ba7522
SaifRehman99 <saifr7493@gmail.com>
2019-05-27 01:38:23 +0500

    Json data added

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-document.querySelector('#getText')
-document.querySelector('#getJson')
-document.querySelector('#getApi')
+document.querySelector('#getText').addEventListener('click', (e) => {
+    fetch('file.txt')
+        .then((res) => res.text())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+
+})
+document.querySelector('#getJson').addEventListener('click', (e) => {
+    fetch('data.json')
+        .then((res) => res.json())
+        .then((data) =>{
+            data.forEach((item)=>{
+                document.querySelector('.json').innerHTML +=`<ul>
+                <li></li></ul>` 
+            })
+        })
+        .catch((err) => console.log(err));
+
+})
+document.querySelector('#getApi').addEventListener('click', (e) => {
+    fetch('file.txt')
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+
+})
